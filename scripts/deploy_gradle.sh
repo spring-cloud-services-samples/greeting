13825defd51728deaa8d52575d9894af1db10646
Ben Klein <bklein@pivotal.io>
2016-04-06 11:22:35 -0500

    Increase sleep time in Bash deployment scripts

--- scripts/deploy_gradle.sh
+++ scripts/deploy_gradle.sh
@@ -1,5 +1,5 @@
 cf create-service p-service-registry standard service-registry
-sleep 40;
+sleep 70;
 pushd message-generation && cf push -p build/libs/message-generation-0.0.1-SNAPSHOT.jar
 popd; sleep 10
 pushd greeter && cf push -p build/libs/greeter-0.0.1-SNAPSHOT.jar
--- scripts/deploy_mvn.sh
+++ scripts/deploy_mvn.sh
@@ -1,5 +1,5 @@
 cf create-service p-service-registry standard service-registry
-sleep 40;
+sleep 70;
 pushd message-generation && cf push -p target/message-generation-0.0.1-SNAPSHOT.jar
 popd; sleep 10
 pushd greeter && cf push -p target/greeter-0.0.1-SNAPSHOT.jar
