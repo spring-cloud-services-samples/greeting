306845abfd173d4438f1f90edbddd7ee68125ed9
Ben Klein <bklein@pivotal.io>
2016-06-29 14:27:35 -0500

    Update Bash scripts for Spring Cloud Services 1.1

--- scripts/deploy_mvn.sh
+++ scripts/deploy_mvn.sh
@@ -1,7 +1,15 @@
-cf create-service p-service-registry standard service-registry
-sleep 70;
+echo -n "Creating Service Registry..."
+{
+  cf create-service p-service-registry standard service-registry
+} &> /dev/null
+until [ `cf service service-registry | grep -c "succeeded"` -eq 1  ]
+do
+  echo -n "."
+done
+echo
+echo "Service Registry created. Pushing applications."
 pushd message-generation && cf push -p target/message-generation-0.0.1-SNAPSHOT.jar
-popd; sleep 10
+popd
 pushd greeter && cf push -p target/greeter-0.0.1-SNAPSHOT.jar
 popd
 echo "" && echo "Done!" && echo ""
