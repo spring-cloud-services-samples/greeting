eb5407161371995dab182ffac1a720a72646580b
Ben Klein <bklein@pivotal.io>
2016-07-18 15:32:39 -0500

    Update Windows deployment scripts

--- scripts/deploy_mvn.bat
+++ scripts/deploy_mvn.bat
@@ -1,9 +1,14 @@
 @echo off
-cf create-service p-service-registry standard service-registry
-pushd message-generation
-cf push -p target/message-generation-0.0.1-SNAPSHOT.jar
-popd
-pushd greeter
-cf push -p target/greeter-0.0.1-SNAPSHOT.jar
-popd
-echo Done!
+echo Creating Service Registry...
+cf create-service p-service-registry standard service-registry > nul 
+:check
+  cf service service-registry | find "succeeded" > nul
+  if errorlevel 1 goto :check
+  echo Service Registry created. Pushing applications.
+  pushd message-generation
+  cf push -p target/message-generation-0.0.1-SNAPSHOT.jar
+  popd
+  pushd greeter
+  cf push -p target/greeter-0.0.1-SNAPSHOT.jar
+  popd
+  echo Done!
