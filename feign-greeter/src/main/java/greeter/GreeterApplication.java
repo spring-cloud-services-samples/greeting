61f0f36042215d86dc8d11ef5d5407e5b143942d
Ben Klein <bklein@pivotal.io>
2018-06-27 16:57:03 -0500

    Updates to Feign Greeter application for SCS 2.0

--- feign-greeter/src/main/java/greeter/GreeterApplication.java
+++ feign-greeter/src/main/java/greeter/GreeterApplication.java
@@ -15,19 +15,14 @@
  */
  package greeter;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @SpringBootApplication
@@ -37,36 +32,15 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 public class GreeterApplication {
 
     @Autowired
-    MessageGenerationClient messageGeneration;
+    MessagesClient messagesClient;
 
     @RequestMapping(value = "/hello", method = GET)
     public String greeting(@RequestParam(value="salutation", defaultValue="Hello") String salutation, @RequestParam(value="name", defaultValue="Bob") String name) {
-      Greeting greeting =  messageGeneration.greeting(name, salutation);
+      Greeting greeting =  messagesClient.greeting(name, salutation);
       return greeting.getMessage();
     }
 
     public static void main(String[] args) {
         SpringApplication.run(GreeterApplication.class, args);
-    }
-
-    private static class Greeting {
-
-        private String message;
-
-        @JsonCreator
-        public Greeting(@JsonProperty("message") String message) {
-            this.message = message;
-        }
-
-        public String getMessage() {
-            return this.message;
-        }
-
-    }
-
-    @FeignClient("https://greeter-messages")
-    interface MessageGenerationClient {
-      @RequestMapping(value = "/greeting", method = GET)
-      Greeting greeting(@RequestParam("name") String name, @RequestParam("salutation") String salutation);
     }
 }
---
+++ feign-greeter/src/main/java/greeter/Greeting.java
@@ -0,0 +1,18 @@
+package greeter;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Greeting {
+
+    private String message;
+
+    @JsonCreator
+    public Greeting(@JsonProperty("message") String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+}
