fe36702e38f36f990789c5ac0f2487f24b64c6b7
t-merten <73176123+t-merten@users.noreply.github.com>
2020-10-20 14:56:56 +0200

    cmd syntax for deploy_mvn.bat

--- scripts/deploy_mvn.bat
+++ scripts/deploy_mvn.bat
@@ -5,10 +5,10 @@ cf create-service p.service-registry standard greeter-service-registry > nul
   cf service greeter-service-registry | find "succeeded" > nul
   if errorlevel 1 goto :check
   echo Service Registry created. Pushing applications.
-  pushd greeter-messages
-  cf push -p target/greeter-messages-0.0.1-SNAPSHOT.jar
+  pushd ..\greeter-messages
+  cf push -p target\greeter-messages-0.0.1-SNAPSHOT.jar
   popd
-  pushd greeter
-  cf push -p target/greeter-0.0.1-SNAPSHOT.jar
+  pushd ..\greeter
+  cf push -p target\greeter-0.0.1-SNAPSHOT.jar
   popd
   echo Done!
