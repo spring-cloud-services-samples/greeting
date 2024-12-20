ab727b076ab99ce9722b909a8bc7557701d49a17
Max Brauer <mbrauer@vmware.com>
2024-12-20 09:06:46 +0100

    tests: use Spring Framework's MockitoBean
    https://github.com/spring-projects/spring-boot/wiki/Spring-Boot-3.4-Release-Notes#deprecation-of-mockbean-and-spybean
    
    Signed-off-by: Max Brauer <mbrauer@vmware.com>

--- greeter/src/test/java/greeter/GreeterControllerTest.java
+++ greeter/src/test/java/greeter/GreeterControllerTest.java
@@ -18,15 +18,13 @@ package greeter;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -40,7 +38,7 @@ public class GreeterControllerTest {
 	@Autowired
 	private MockMvc mvc;
 
-	@MockBean
+	@MockitoBean
 	private GreeterService greeterService;
 
     @WithMockUser(value = "Cookie")
