f6ff49b286cee57d86ec318161fa7f0a2ef65e05
kvmw <mshamsi@broadcom.com>
2025-11-26 14:49:00 +0100

    Bumps spring boot to v4.0.0 and scs starters to v4.4.0
    Signed-off-by: kvmw <mshamsi@broadcom.com>

--- greeter-messages/src/test/java/messages/MessagesControllerTest.java
+++ greeter-messages/src/test/java/messages/MessagesControllerTest.java
@@ -20,7 +20,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
